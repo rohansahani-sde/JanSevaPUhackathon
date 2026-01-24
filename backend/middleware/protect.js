25580e41f95d6c3b1b6a2e68633377449b06e6e5
rohan <sahanirohan313@gmail.com>
2026-01-24 06:42:52 +0530

    Merged rohan_code into main

---
+++ backend/middleware/protect.js
@@ -0,0 +1,8 @@
+export const protect = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "No token" });
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = decoded;
+  next();
+};
