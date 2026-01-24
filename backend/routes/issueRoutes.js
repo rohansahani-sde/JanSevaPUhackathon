e3af530f5f3cf2b517d0b90c9fcc8ded81a8e58c
rohan <sahanirohan313@gmail.com>
2026-01-24 06:40:14 +0530

    fix routes & name

--- backend/routes/issueRoutes.js
+++ backend/routes/issueRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import upload from "../middleware/upload.js";
 import { createIssue, getAllIssues, getUserIssues, getStats, getIssueById, updateIssueStatus,uploadWorkProof, getWorkerIssues } from "../controller/issueController.js";
+import { protect } from "../middleware/protect.js";
 
 
 const router = express.Router();
@@ -11,10 +12,10 @@ router.get("/worker", getWorkerIssues);
 
 router.get("/user/:username", getUserIssues);
 router.get("/all", getAllIssues);
-router.get("/stats", getStats);
-router.get("/:id", getIssueById);
+router.get("/stats", protect, getStats);
+router.get("/:id", protect, getIssueById);
 
-router.put("/:id/status", updateIssueStatus);
+router.put("/:id/status", protect, updateIssueStatus);
 
 router.put("/:id/work-proof", upload.single("image"), uploadWorkProof );
 
